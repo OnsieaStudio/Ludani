5643798442dd00ef0534725b0c77dba2fd5afbff
alex zaros <seyronax@gmail.com>
2021-12-24 19:29:35 +0100

    Physical and symbolic links corrections of source folders and files
    Replacement of some symbolic links in hard links and junction

---
+++ sources/common/fr/onsiea/engine/common/module-info.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+/**
+ * @author seyro
+ *
+ */
+module fr.onsiea.engine.common
+{
+	exports fr.onsiea.engine.common;
+	exports fr.onsiea.engine.common.game;
+	exports fr.onsiea.engine.server;
+	exports fr.onsiea.engine.client;
+	exports fr.onsiea.engine.utils.time;
+
+	requires lombok;
+}
