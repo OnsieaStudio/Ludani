2203f43ab96c2adc67eebb146a52717c50422863
alex zaros <seyronax@gmail.com>
2022-02-21 20:20:33 +0100

    Time and date utils

---
+++ sources/utils/fr/onsiea/engine/utils/time/DateUtils.java
@@ -0,0 +1,55 @@
+/**
+*	Copyright 2021 Onsiea All rights reserved.
+*
+*	This file is part of OnsieaLogger. (https://github.com/Onsiea/OnsieaLogger)
+*
+*	Unless noted in license (https://github.com/Onsiea/OnsieaLogger/blob/main/LICENSE.md) notice file (https://github.com/Onsiea/OnsieaLogger/blob/main/LICENSE_NOTICE.md), OnsieaLogger and all parts herein is licensed under the terms of the LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)  found here https://www.gnu.org/licenses/lgpl-3.0.html and copied below the license file.
+*
+*	OnsieaLogger is libre software: you can redistribute it and/or modify
+*	it under the terms of the GNU Lesser General Public License as published by
+*	the Free Software Foundation, either version 3.0 of the License, or
+*	(at your option) any later version.
+*
+*	OnsieaLogger is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU Lesser General Public License for more details.
+*
+*	You should have received a copy of the GNU Lesser General Public License
+*	along with OnsieaLogger.  If not, see <https://www.gnu.org/licenses/> <https://www.gnu.org/licenses/lgpl-3.0.html>.
+*
+*	Neither the name "Onsiea", "OnsieaLogger", or any derivative name or the names of its authors / contributors may be used to endorse or promote products derived from this software and even less to name another project or other work without clear and precise permissions written in advance.
+*
+*	(more details on https://github.com/Onsiea/OnsieaLogger/wiki/License)
+*
+*	@author Seynax
+*/
+package fr.onsiea.engine.utils.time;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author Seynax
+ *
+ */
+public class DateUtils
+{
+	private final static SimpleDateFormat DEFAULT_FORMAT = new SimpleDateFormat("yyyy.MM.dd");
+
+	public final static String str()
+	{
+		return DateUtils.DEFAULT_FORMAT.format(new Date());
+	}
+
+	public final static String str(String formatIn)
+	{
+		return new SimpleDateFormat(formatIn).format(new Date());
+	}
+
+	public final static String str(DateFormat dateFormatIn)
+	{
+		return dateFormatIn.format(new Date());
+	}
+}
