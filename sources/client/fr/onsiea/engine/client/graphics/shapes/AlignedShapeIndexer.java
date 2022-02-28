c91a9415dc508137c72a3ca7941590bcbc00fd32
alex zaros <seyronax@gmail.com>
2022-02-28 09:25:11 +0100

    Restructuring of the cube shape class

--- sources/client/fr/onsiea/engine/client/graphics/shapes/AlignedShapeIndexer.java
+++ sources/client/fr/onsiea/engine/client/graphics/shapes/AlignedShapeIndexer.java
@@ -26,7 +26,7 @@
 */
 package fr.onsiea.engine.client.graphics.shapes;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.joml.Vector2f;
@@ -38,31 +38,27 @@ import org.joml.Vector3f;
  */
 public class AlignedShapeIndexer
 {
-	private final static List<C>		cs		= new ArrayList<>();
-	private final static List<Integer>	indices	= new ArrayList<>();
+	private final static List<C>		cs		= new LinkedList<>();
+	private final static List<Integer>	indices	= new LinkedList<>();
 
-	static
+	public final static void runtime()
 	{
-		var i0 = 0;
-		for (var i = 0; i < Cube.allPositionsAndTextureCoordinatesAndNormals.length; i += 3 + 2 + 3)
+		var	i0	= 0;
+		var	i1	= 0;
+		for (var i = 0; i < Cube.allPositions.length;)
 		{
-			final var	x	= Cube.allPositionsAndTextureCoordinatesAndNormals[i];
-			final var	y	= Cube.allPositionsAndTextureCoordinatesAndNormals[i + 1];
-			final var	z	= Cube.allPositionsAndTextureCoordinatesAndNormals[i + 2];
-			final var	ux	= Cube.allPositionsAndTextureCoordinatesAndNormals[i + 3];
-			final var	uy	= Cube.allPositionsAndTextureCoordinatesAndNormals[i + 4];
-			final var	nx	= Cube.allPositionsAndTextureCoordinatesAndNormals[i + 5];
-			final var	ny	= Cube.allPositionsAndTextureCoordinatesAndNormals[i + 6];
-			final var	nz	= Cube.allPositionsAndTextureCoordinatesAndNormals[i + 7];
+			final var x = Cube.allPositions[i];
+			i++;
+			final var y = Cube.allPositions[i];
+			i++;
+			final var z = Cube.allPositions[i];
+			i++;
 
-			// final var	str	= x + " " + y + " " + z + " " + ux + " " + uy + " " + nx + " " + ny + " " + nz + " ";
-
-			C			c	= null;
+			C c = null;
 
 			for (final C c0 : AlignedShapeIndexer.cs)
 			{
-				if (c0.position.x == x && c0.position.y == y && c0.position.z == z && c0.texCoords.x == ux
-						&& c0.texCoords.y == uy && c0.normal.x == nx && c0.normal.y == ny && c0.normal.z == nz)
+				if (c0.position.x == x && c0.position.y == y && c0.position.z == z)
 				{
 					c = c0;
 
@@ -74,11 +70,30 @@ public class AlignedShapeIndexer
 			{
 				c			= new C();
 				c.position	= new Vector3f(x, y, z);
-				c.texCoords	= new Vector2f(ux, uy);
-				c.normal	= new Vector3f(nx, ny, nz);
 				c.index		= i0;
 				i0++;
 				AlignedShapeIndexer.cs.add(c);
+			}
+
+			System.out.print(c.index);
+
+			if (i + 3 <= Cube.allPositions.length)
+			{
+				System.out.print(",");
+			}
+			if (i1 <= 1)
+			{
+				System.out.print(" ");
+			}
+
+			if (i1 >= 2)
+			{
+				System.out.println();
+				i1 = 0;
+			}
+			else
+			{
+				i1++;
 			}
 
 			AlignedShapeIndexer.indices.add(c.index);
@@ -86,19 +101,17 @@ public class AlignedShapeIndexer
 
 		for (final C c : AlignedShapeIndexer.cs)
 		{
-			System.out.println(c.position.x() + "f, " + c.position.y() + "f, " + c.position.z() + "f, "
-					+ c.texCoords.x() + "f, " + c.texCoords.y() + "f, " + c.normal.x() + "f, " + c.normal.y() + "f, "
-					+ c.normal.z() + "f, ");
+			System.out.println(c.position.x() + "f, " + c.position.y() + "f, " + c.position.z());
 		}
 
-		for (var i = 0; i < AlignedShapeIndexer.indices.size(); i += 3)
+		/**for (var i = 0; i < AlignedShapeIndexer.indices.size(); i += 3)
 		{
 			final int	a0	= AlignedShapeIndexer.indices.get(i);
 			final int	a1	= AlignedShapeIndexer.indices.get(i + 1);
 			final int	a2	= AlignedShapeIndexer.indices.get(i + 2);
-
+		
 			System.out.println(a0 + ", " + a1 + ", " + a2 + ",");
-		}
+		}**/
 	}
 
 	final static class C
